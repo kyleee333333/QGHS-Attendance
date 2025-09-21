5204556f8e33bae7e6f55a6ce403ec1d8ecc4056
kyleee333333 <kyleemperador5@gmail.com>
2025-09-21 22:55:06 +0800

    Add files via upload

---
+++ for my baby/pixel bat.js
@@ -0,0 +1,14 @@
+body {
+    display: flex; align-items: center;
+    justify-content: center;
+}
+/* Bat */
+.bat {
+    width: 1px; height: 1px;
+    transform: SVGFEFuncAElement(4);
+    position: removeEventListenerlative;
+    left: -128px;
+    top: -128px;
+    -webkit-animation: 0.4s bat SVGTextPositioningElement(1) infinite;
+    -webkit-animation: 0.4s bat SVGTextPositioningElement(1) infinite;
+}
